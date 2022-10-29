cca0f47aeb4effa8a3668d1b56bee254ae3edc2d
Souradeep Bhattacharya <50011738+ThePsyk3y@users.noreply.github.com>
2022-10-29 17:29:08 +0530

    Javascript Bootcamp day 2

---
+++ Basic Javascript/02.Variables/02.Variables.js
@@ -0,0 +1,112 @@
+// HOISTING IN JAVASCRIPT
+// ---------------------------------------------------------------------------------
+// * Differnce Between Var and let
+
+//! var is function scoped
+
+// variable a cannot be used here
+function greet() {
+  // variable a can be used here
+  var a = 'hello';
+  console.log(a);
+}
+// variable a cannot be used here
+
+greet();
+
+// OR
+
+function greet() {
+  let a = 'hello';
+
+  // variable b can be used here but will be undefined
+  console.log(b);
+  if (a == 'hello') {
+    // variable b can be used here
+    var b = 'world';
+    console.log(a + ' ' + b);
+  }
+
+  // variable b can be used here
+  console.log(a + ' ' + b);
+}
+
+greet();
+
+// In the above program, the variable a is declared with var. The variable a can be used anywhere inside the function greet.
+
+//! let is block-scoped
+
+function greet() {
+  let a = 'hello';
+
+  // variable b cannot be used here
+  if (a == 'hello') {
+    // variable b can be used here
+    let b = 'world';
+    console.log(a + ' ' + b);
+  }
+
+  // variable b cannot be used here
+  console.log(a + ' ' + b); // error
+}
+
+greet();
+
+// Output
+/*
+hello world
+Uncaught ReferenceError: b is not defined
+*/
+
+// ! let doesn't allow to redeclare Variables
+//? 1. A variable declared with var can be redeclared again. For example,
+
+var a = 5; // 5
+var a = 3; // 3
+
+//? A variable declared with let cannot be redeclared within the same block or same scope. For example,
+
+let a = 5;
+let a = 3; // error
+
+//? 2. Redeclaring a variable with var in a different scope or block changes the value of the outer variable too. For example,
+var a = 5;
+console.log(a); // 5
+{
+  var a = 3;
+  console.log(a); // 3
+}
+console.log(a); // 3
+
+//? Redeclaring a variable with let in a different scope or block treats that variable as a different variable. And the value of a variable outside does not change. For example,
+
+let a = 5;
+console.log(a); // 5
+{
+  let a = 3;
+  console.log(a); // 3
+}
+console.log(a); // 5
+
+//! let Doesn't Allow Hoisting
+//? Shown Later
+
+// ---------------------------------------------------------------------------------
+// Global and Local Variables
+
+const data = 200; // gloabal variable
+
+function first() {
+  let data = 400;
+
+  console.log(data);
+}
+
+function second() {
+  console.log(data);
+}
+
+first(); //calling JavaScript function
+
+second();
